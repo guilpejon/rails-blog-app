c38a6bbd7a49430b56fe49fc07a64f70f395ed95
Guilherme Pejon <guilpejon@gmail.com>
2023-06-17 01:27:07 -0300

    Added bootstrap to devise

--- project-9-facebook/app/helpers/application_helper.rb
+++ project-9-facebook/app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
   def flash_class(level)
-   return 'alert alert-info' if level == 'notice'
-   return 'alert alert-success' if level == 'success'
-   return 'alert alert-error' if level == 'error'
-   return 'alert alert-error' if level == 'error'
+    return 'alert alert-info' if level == 'notice'
+    return 'alert alert-success' if level == 'success'
+    return 'alert alert-error' if level == 'error'
+    return 'alert alert-error' if level == 'error'
+
+    'alert alert-info'
   end
 end
--- project-9-facebook/db/seeds.rb
+++ project-9-facebook/db/seeds.rb
@@ -1,10 +1,14 @@
+User.destroy_all
+
 30.times do
   user = User.create(
-    name: Faker::Name.name,
+    name: Faker::TvShows::TheOffice.character,
     email: Faker::Internet.email,
     password: 'password' # Replace with the desired default password for the generated users
   )
-  url = URI.parse(Faker::LoremFlickr.image)
+  url = URI.parse(Faker::Avatar::image)
   downloaded_image = URI.open(url)
   user.profile_pic.attach(io: downloaded_image, filename: 'profile_pic.jpg')
 end
+
+puts User.all.pluck(:email)
